b85d115da48387a625ab8bcfb8398d92e3160664
VCHEPKOIT <vchepkoit@safaricom.co.ke>
2019-05-26 21:36:07 +0300

    first commit

---
+++ src/main/java/App.java
@@ -0,0 +1,12 @@
+import models.Departments;
+import models.Staff;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import static spark.Spark.*;
+import java.util.Map;
+
+
+
